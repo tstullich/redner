6021b67d577bedd7100718e3a99dcf3442b11ab3
Tim Stullich <tstullich@protonmail.com>
2019-09-03 19:39:49 +0200

    Added phase spherical direction function

--- phase_function.h
+++ phase_function.h
@@ -27,7 +27,7 @@ struct HeyneyGreenstein : PhaseFunction {
 
     // This function works much like p() but it is extended
     // to use a sample in the range of [0, 1)^2 to perform MIS
-    float sample_p(const Vector3f &wo, const Vector3f *wi,
+    float sample_p(const Vector3f &wo, Vector3f *wi,
                    const Vector2f &sample) const {
         // Compute cosine theta
         float cosTheta;
@@ -40,10 +40,11 @@ struct HeyneyGreenstein : PhaseFunction {
 
         // Compute the direction wi based on the HG phase function
         float sinTheta = std::sqrt(std::max(0.0f, 1.0f - cosTheta * cosTheta));
-        float phi = 2.0 * M_PI * sample[1];
+        float phi = 2.0f * M_PI * sample[1];
         Vector3f v1, v2;
-        // TODO lookup CoordinateSystem function and SphericalDirection
-
+        // TODO Check if coordinateSystem function and sphericalDirection are
+        // needed coordinate_system();
+        *wi = sphericalDirection(sinTheta, cosTheta, phi, v1, v2, -wo);
         return PhaseHG(-cosTheta, g);
     }
 
@@ -53,6 +54,16 @@ struct HeyneyGreenstein : PhaseFunction {
     inline float PhaseHG(float cosTheta, float g) const {
         float denom = 1.0f + g * g + 2.0f * g * cosTheta;
         return INV_PI * (1.0f - g * g) / (denom * std::sqrt(denom));
+    }
+
+    // Calculates the new direction of the outgoing vector given three basis
+    // vectors
+    inline Vector3f sphericalDirection(float sinTheta, float cosTheta,
+                                       float phi, const Vector3f &x,
+                                       const Vector3f &y,
+                                       const Vector3f &z) const {
+        return sinTheta * std::cos(phi) * x + sinTheta * std::sin(phi) * y +
+               cosTheta * z;
     }
 
     const float g;
